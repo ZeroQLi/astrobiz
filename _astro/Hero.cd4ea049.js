2efb3bd9a6e345fe82838abfd75d98708f6fb575
ZeroQLi <itsrandomboy77@gmail.com>
2024-02-28 11:46:34 +0000

    Deploying to gh-pages from @ ZeroQLi/millaskytrading@2ad3139a861683c2b155643207429dd637e20ae0 🚀

---
+++ _astro/Hero.cd4ea049.js
@@ -0,0 +1,8 @@
+/* empty css                       */import{j as e}from"./jsx-runtime.73bdaf71.js";import"./index.45a47ed6.js";const s=`
+    .main {
+        background-image: url(./office.jpg);
+        background-size: 100% 100%;
+        height:75vh;
+        width:100%;
+    }
+`,a=()=>e.jsxs("section",{className:"main",children:[e.jsxs("div",{className:"container mx-auto flex px-10 py-20 md:flex-row flex-col items-center",children:[e.jsx("div",{className:"lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0",children:e.jsx("img",{className:"object-cover object-center rounded",alt:"hero",src:"./factory.jpg"})}),e.jsxs("div",{className:"lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center",children:[e.jsx("h1",{className:"title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900",children:"Millasky General Trading"}),e.jsx("p",{className:"mb-8 leading-relaxed",children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus facilisis, metus nec faucibus rhoncus, nunc enim sodales nisl, vel lobortis nisl metus vel nulla. Integer vitae congue dolor. Aenean auctor vitae mauris sit amet commodo. Phasellus viverra lectus vitae ultrices vulputate."})]})]}),e.jsx("style",{children:s})]});export{a as default};
