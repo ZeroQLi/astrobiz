c69b91ebabf35a80fd3e410fca59d6afe49cf609
ZeroQLi <itsrandomboy77@gmail.com>
2024-02-28 11:57:18 +0000

    Deploying to gh-pages from @ ZeroQLi/millaskytrading@2f9ca70e79257fcbc2c1109f2445494fd982d444 🚀

---
+++ _astro/Hero.a6702625.js
@@ -0,0 +1,8 @@
+/* empty css                       */import{j as e}from"./jsx-runtime.73bdaf71.js";import"./index.45a47ed6.js";const t=`
+    .main {
+        background-image: url(./building.jpg);
+        background-size: 100% 100%;
+        height:90vh;
+        width:100%;
+    }
+`,a=()=>e.jsxs("section",{className:"main",children:[e.jsxs("div",{className:"container mx-auto flex px-10 py-20 md:flex-row flex-col items-center",children:[e.jsx("div",{className:"lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0",children:e.jsx("img",{className:"object-cover object-center rounded",alt:"hero",src:"./factory.jpg"})}),e.jsxs("div",{className:"bg-black rounded-lg m-4 p-8 bg-opacity-65 lg:flex-grow md:w-1/2 lg:pl-12 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center",children:[e.jsx("h1",{className:"title-font sm:text-4xl text-3xl mb-4 font-medium text-white",children:"Millasky General Trading"}),e.jsx("p",{className:" text-slate-50 mb-8 leading-relaxed",children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, exercitationem reprehenderit, porro ad vel ratione maiores deleniti laborum tempore culpa animi, nisi mollitia doloremque! Soluta tenetur deleniti omnis iure tempora! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus facilisis, metus nec faucibus rhoncus, nunc enim sodales nisl, vel lobortis nisl metus vel nulla. Integer vitae congue dolor. Aenean auctor vitae mauris sit amet commodo. Phasellus viverra lectus vitae ultrices vulputate."})]})]}),e.jsx("style",{children:t})]});export{a as default};
