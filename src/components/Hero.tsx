169a62703e066943d1e4a4b737c876aeee938e42
ZeroQLi <itsrandomboy77@gmail.com>
2024-02-28 17:59:16 +0400

    mobile fix test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,12 +21,11 @@ const Hero = () => {
  <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
  <img className="object-cover object-center rounded" alt="hero" src="./factory.jpg" />
  </div>
- <div className="bg-black rounded-lg m-4 p-8 bg-opacity-65 lg:flex-grow md:w-1/2 lg:pl-12 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+ <div className="bg-black rounded-lg lg:m-4 lg:p-8 bg-opacity-65 lg:flex-grow md:w-1/2 lg:pl-12 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
  <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
  Millasky General Trading
  </h1>
  <p className=" text-slate-50 mb-8 leading-relaxed">
- Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, exercitationem reprehenderit, porro ad vel ratione maiores deleniti laborum tempore culpa animi, nisi mollitia doloremque! Soluta tenetur deleniti omnis iure tempora!
  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus facilisis, metus nec faucibus rhoncus, nunc enim sodales nisl, vel lobortis nisl metus vel nulla. Integer vitae congue dolor. Aenean auctor vitae mauris sit amet commodo. Phasellus viverra lectus vitae ultrices vulputate.
  </p>
  </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,22 +3,22 @@ import { Link } from 'react-scroll';
 
 const Navbar = () => {
  return (
- <nav className=" bg-red-100 shadow-lg">
+ <nav className="sm:w-full bg-red-100 shadow-lg">
   <div className="container mx-auto py-3">
   <div className="flex items-center justify-between ">
-  <div className="flex items-center ">
+  <div className="hidden lg:block flex items-center">
   
   <a className="m-0" href="/">
     <img className=' h-10 ' src='/logo.png'></img>
   </a>
   </div>
   <div className="flex items-center">
-  <a href="/" className=" text-slate-800 hover:text-gray-900 p-2 mx-3 cursor-pointer">Home</a>
-           <a href="/about_us" className="text-slate-800 hover:text-gray-900 p-2 mx-3 cursor-pointer">About us</a>
-           <a href="/products" className="text-slate-800 rounded hover:text-gray-900 p-2 mx-3 cursor-pointer">Products</a>
+    <a href="/" className="text-xs lg:text-base text-slate-800 hover:text-gray-900 p-2 mx-3 cursor-pointer">Home</a>
+           <a href="/about_us" className="text-xs lg:text-base text-slate-800 hover:text-gray-900 p-2 mx-3 cursor-pointer">About us</a>
+           <a href="/products" className="text-xs lg:text-base text-slate-800 rounded hover:text-gray-900 p-2 mx-3 cursor-pointer">Products</a>
   <button
     type="button"
-    className="inline-block rounded mx-3 bg-yellow-400 px-6 pb-2 pt-2.5 text-s font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+    className="text-xs lg:text-base inline-block rounded mx-3 bg-yellow-400 px-6 pb-2 pt-2.5 text-s font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
     <a href="#contact">Contact us</a>
   </button>
   </div>
