4ae21344058f7eec1cf18a44b5989a3584083e83
ZeroQLi <itsrandomboy77@gmail.com>
2024-03-19 19:24:18 +0000

    Deploying to gh-pages from @ ZeroQLi/millaskytrading@e88831b88b81f257be59415130dd3f056832130f 🚀

---
+++ _astro/Hero.DKqWRs9d.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime.K1e75nIr.js";import"./index.NEDEFKed.js";import"./styles.DkNzp3eg.js";import"./_commonjsHelpers.Cpj98o6Y.js";const t=`
+    .main {
+        @media only screen and (max-width: 499px) {
+            height: 40vh;
+        }
+        
+        background-image: url(./office.jpg);
+        background-size: 100% 100%;
+        height:90vh;
+        width:100%;
+        
+    }
+`,r=()=>e.jsxs("section",{className:" main",children:[e.jsx("div",{className:"container mx-auto flex px-10 py-20 md:flex-row flex-col items-center",children:e.jsx("div",{className:" bg-opacity-60 max-sm:mt-28 lg:mt-80 bg-black rounded-lg lg:m-4 lg:p-8 lg:flex-grow md:w-1/2 lg:pl-12 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center",children:e.jsx("p",{className:"text-xs lg:text-4xl p-2 text-left text-slate-50 mb-0 leading-relaxed",children:"Millasky trading FZC is a multinational trading company in Sharjah, United Arab Emirates. We specialize in sale of non-essential products. We are best known for our range of home care and body care products"})})}),e.jsx("style",{children:t})]});export{r as default};
